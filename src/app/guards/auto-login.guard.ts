0f81e21a8ac2f350c175dcddc9b0c86d0c166c66
esl4m <me@esl4m.com>
2020-12-13 00:50:22 +0300

    Adds automatic Login - if the current user is authenticated

--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Router, Route, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { filter, map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutoLoginGuard implements CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    if (this.authService.isAuthenticated){
+      return this.authService.isAuthenticated.pipe(
+        filter(val => val !== null), // Filter out initial Behaviour subject value
+        take(1), // Otherwise the Observable doesn't complete!
+        map(isAuthenticated => {
+          console.log('Found previous token, automatic login');
+          if (isAuthenticated) {
+            this.router.navigateByUrl('/home', { replaceUrl: true });
+          } else {
+            // Simply allow access to the login
+            return true;
+          }
+        })
+      );
+    }
+    else {
+      this.router.navigateByUrl('/home');
+    }
   }
 }

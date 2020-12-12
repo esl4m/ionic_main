f53273918d146d7f6d08f96f08e6eff35c14218e
esl4m <me@esl4m.com>
2020-12-12 16:02:26 +0300

    Adds cool login - with FormBuilder - with default values

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  credentials: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private alertController: AlertController,
+    private router: Router,
+    private loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['me@esl4m.com', [Validators.required, Validators.email]],
+      password: ['123', [Validators.required, Validators.minLength(1)]],
+    });
+  }
+
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    
+    this.authService.login(this.credentials.value).subscribe(
+      async (res) => {
+        await loading.dismiss();        
+        this.router.navigateByUrl('/folder/inbox', { replaceUrl: true });
+      },
+      async (res) => {
+        await loading.dismiss();
+        const alert = await this.alertController.create({
+          header: 'Login failed',
+          message: res.error.error,
+          buttons: ['OK'],
+        });
+ 
+        await alert.present();
+      }
+    );
+  }
+
+  // Easy access for form fields
+  get email() {
+    return this.credentials.get('email');
+  }
+  
+  get password() {
+    return this.credentials.get('password');
   }
 
 }

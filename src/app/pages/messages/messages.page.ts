c7fb7ed49304cf04657cd0b02bc547f84f7d085f
esl4m <me@esl4m.com>
2020-12-19 20:12:34 +0300

    Adds list Messages , adds html list messages and checks

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-messages',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesPage implements OnInit {
 
-  constructor() { }
+  allMessages: any;
+  messageDetails: any;
+  AUTH_SERVER_ADDRESS: string = environment.apiURL;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.getMessages();
   }
 
+  getMessages(){
+    if (this.authService.isAuthenticated) {
+      this.http.get(`${this.AUTH_SERVER_ADDRESS}/messages`)
+      .subscribe((res:any) => {
+        if (res.messages){
+          console.log(res.messages);
+          this.allMessages = res.messages;
+        }
+      },
+      (err) => {
+        console.log(err);
+      });
+    }
+    else {
+      console.log('user logged out!');
+      this.router.navigateByUrl('/login', { replaceUrl: true });
+    }
+  }
 }
